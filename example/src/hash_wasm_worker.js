7b173ed20d749da1a507c2cdbeb1d42ed9a2685f
tkasuz <tkasuz.dev@gmail.com>
2024-10-22 14:51:19 +0900

    chore: compare to hash-wasm

--- example/src/hash_wasm_worker.js
+++ example/src/hash_wasm_worker.js
@@ -1,8 +1,8 @@
-import {from_blob} from 'md5gen-wasm'
+import {md5} from 'hash-wasm'
 
 onmessage = async (e) => {
     const t0 = performance.now();
-    const hex = await from_blob(e.data)
+    const hex = await md5(new Uint8Array(await e.data.arrayBuffer()));
     const t1 = performance.now();
     const latency = t1 - t0
     const throughput = Math.floor(e.data.size / 1024 / 1024 / (t1 - t0) * 1000)
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,16 +1,49 @@
 import Worker from './worker.js?worker'
+import HashWasmWorker from './hash_wasm_worker.js?worker'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <input type="file" id="file">
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Library</th>
+          <th scope="col">Latency [ms]</th>
+          <th scope="col">Throughput [MB/s]</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">md5gen-wasm</th>
+          <td id="md5gen-wasm-latency"></td>
+          <td id="md5gen-wasm-throughput"></td>
+        </tr>
+        <tr>
+          <th scope="row">hash-wasm</th>
+          <td id="hash-wasm-latency"></td>
+          <td id="hash-wasm-throughput"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 `
 
 const file = document.getElementById('file');
 const worker = new Worker();
+const hashWasmWorker = new HashWasmWorker();
+const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
 
 worker.onmessage = (e) => {
   console.log(e.data);
+  document.getElementById('md5gen-wasm-latency')!.textContent = e.data[1];
+  document.getElementById('md5gen-wasm-throughput')!.textContent = e.data[2];
+};
+
+hashWasmWorker.onmessage = (e) => {
+  console.log(e.data);
+  document.getElementById('hash-wasm-latency')!.textContent = e.data[1];
+  document.getElementById('hash-wasm-throughput')!.textContent = e.data[2];
 };
 
 if (file) {
@@ -18,6 +51,8 @@ if (file) {
     const target = event.target as HTMLInputElement;
     if (target && target.files) {
       worker.postMessage(target.files[0]);
+      await sleep(3000);
+      hashWasmWorker.postMessage(target.files[0]);
     }
   });
 };
